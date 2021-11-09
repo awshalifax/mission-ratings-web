e75fa60b75388d01cfdbe898883d678b055af05b
William <awshalifax@gmail.com>
2021-11-08 21:33:13 -0400

    testing again..

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure RedHat Openshift FTW!
+        Azure RedHat Openshift FTW
       </div>
       <div class="col-lg-6">
       </div>
